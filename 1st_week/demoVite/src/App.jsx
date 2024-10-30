f6cbd3cf38ba3b59c00e19802d76e1ab7cda38a4
Jana Stefanie <s2400789@BC-CWRW4DVTW1-Kannettava-tietokone.local>
2024-10-30 21:20:31 +0200

    add new note file

--- 1st_week/demoVite/src/App.jsx
+++ 1st_week/demoVite/src/App.jsx
@@ -1,12 +1,20 @@
 import './App.css'
 import Card from './Card.jsx'
+import { useState } from 'react';
 
 function App() {
+
+  const [person, setPerson] = useState({
+    firstname:"Ramona", 
+    title:"Developer", 
+    age:52,
+  })
+
   return (
     <div>
     <header>Logo</header>
     <main>
-    <Card firstName="Steffi" title="Student" age="29"/>
+    <Card firstname={person.firstName} title={person.title} age={person.age}/>
     <Card firstName="Dave" title="CEO" age="25"/>
     <Card firstName="Mary" title="Developer" age="45"/>
     <Card firstName="Bert" title="Developer" age="35"/>
--- 1st_week/demoVite/src/main.jsx
+++ 1st_week/demoVite/src/main.jsx
@@ -3,13 +3,14 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-const name = 'Steffi';
+const name = "Steffi";
 const first_page = 2019;
 
-const demo = (<div>
+const demo = (
+<div>
   <h1>Hello World! My name is {name}</h1>
-  <p>I have {2024-first_page} years of building webpages experience.</p>
-  </div>)
+  <p>I have {2024 - first_page} years of building webpages experience.</p>
+  </div>);
 
 // capital D marks the component:
   const Demo = () => {
@@ -18,11 +19,11 @@ const demo = (<div>
       <p>I have {2024-first_page} years of building webpages experience.</p>
       </div>
 
-    )
-  }
+    );
+  };
 // here we are rendering: 
-createRoot(document.getElementById('root')).render( // createRoot is a method defined inside react-dom
+createRoot(document.getElementById("root")).render( // createRoot is a method defined inside react-dom
   <StrictMode>
     <App/>   
-  </StrictMode>,
-)
+  </StrictMode>
+);
